b58a7a835f80f354b6955e85c0e7cdb875f4ed14
Kashif <kashifimran2008@gmail.com>
2023-03-15 21:29:28 +0500

    conflict

--- src/scripts/app/site/pages/home.js
+++ src/scripts/app/site/pages/home.js
@@ -3,6 +3,7 @@ import getComp from 'setjs/template/component.js';
 export default {
   templates: ['site/home'],
   comp: function() {
+    console.log('conflict');
     return getComp('site/home');
   }
 };
