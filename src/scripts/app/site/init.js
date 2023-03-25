13a0a43904273133bd4b589539c43215bc8ff1f0
zoya rasheed <zr5530213@gmail.com>
2023-03-25 12:18:55 +0500

    Another page

--- src/scripts/app/site/init.js
+++ src/scripts/app/site/init.js
@@ -3,8 +3,10 @@ import {addPage, addPaths} from 'setjs/kernel/page-manager.js';
 import './api.js';
 import './template-functions.js';
 import home from './pages/home.js';
+import mission from './pages/mission.js'
 
 export default function() {
   addPage('', home);
+  addPage('mission',mission)
   addPaths('site', []);
 }
